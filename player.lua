59bb801f7e7fd78590f5885919443a9027f71688
Demian <demianmcaceres@gmail.com>
2021-02-04 03:21:25 -0300

    replaced global variables with local added recording and playing player elements refactor of functions added recover life function

--- player.lua
+++ player.lua
@@ -1,9 +1,9 @@
 local player = {}
-local rb, rbs, rw, rws, rd, fc = memory.readbyte, memory.readbytesigned, memory.readword, memory.readwordsigned, memory.readdword, emu.framecount
 
 function player.new(number)
 	return  {
 		number = number,
+		side = 0,
 		health = { 
 			current = 0,
 			previous = 0 
@@ -27,20 +27,60 @@ function player.new(number)
 		inputs = {
 			current = {},
 			previous = {},
-			history = {-1}
+			history = {-1},
+			record = {
+				enabled = false,
+				memory = {},
+				index = 1,
+				side = 0,
+				current = {}
+			},
+			playback = {
+				enabled = false,
+				memory = {},
+				index = 1
+			},
 		},
-		buttons = inputs.getButtons(number),
 		memory = {},
 		memory2 = {}
 	}
 end
 
-function player.set(player, key, value)
-	player[key] = value
+local function getBase(playerNumber, game)
+	return game.address.player + (playerNumber - 1) * game.address.space
 end
 
-function player.get(player, key)
-	return player[key]
+local function getDamage(player)
+	local current = player.health.current
+	local previous = player.health.previous
+	local damage = player.damage.hit
+	if (previous > current) then
+		damage = math.abs(previous - current)
+	end
+	return damage
+end
+
+local function checkMaxValue(value, max)
+	if value > max then
+		value = 0
+	end
+	return value
+end
+
+function player.recoverLife(player)
+	local data = player.game.data
+	local base = getBase(player.number, player.game)
+	local address = player.game.cheats.health.address
+	local value = player.game.cheats.health.value
+	local current = player.health.current 
+
+	if current <= 20 then
+		writeWord(base + address, value)
+	end
+end
+
+function player.set(player, key, value)
+	player[key] = value
 end 
 
 function player.getNumber(player)
@@ -51,26 +91,22 @@ function player.getGameData(player)
 	return player.game.data
 end 
 
-local function getBase(playerNumber, game)
-	return game.address.player + (playerNumber - 1) * game.address.space
-end
-
 function player.update(player)
-	data = player.game.data
-	base = getBase(player.number, player.game)
+	local data = player.game.data
+	local base = getBase(player.number, player.game)
+
+	player.side = data.side(base)
 
 	player.health.previous = player.health.current
-	player.health.current = data.health.value(base)
+	player.health.current = checkMaxValue(data.health.value(base), data.health.max)
 	
-	if (player.health.previous > player.health.current) then
-		player.damage.hit = math.abs(player.health.previous - player.health.current)
-	end
+	player.damage.hit = checkMaxValue(getDamage(player), data.damage.max)
 
 	player.guard.previous = player.guard.current
 	player.guard.current = data.guard.value(base)
 	
 	player.stun.previous = player.stun.current
-	player.stun.current = data.stun.value(base)
+	player.stun.current = checkMaxValue(data.stun.value(base), data.stun.max)
 
 	player.super.value = data.super.value(base)
 	player.super.timeout = data.super.timeout.value(base)
