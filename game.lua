d3aeac2e7c498e062fe0295f89660debdde9c050
Demian <demianmcaceres@gmail.com>
2021-02-04 03:17:49 -0300

    replaced global variables with local

--- game.lua
+++ game.lua
@@ -1,17 +1,16 @@
 local game = {}
-profile = require 'profile'
 
 function game.getData()
-	game = nil
+	local game = nil
 	for _, module in ipairs(profile.get()) do
 		for _, shortname in ipairs(module.name) do
 			if emu.romname() == shortname or emu.parentname() == shortname then
 				print("Game: " .. emu.gamename())
-				game = module
-				return game 
+				game = module 
 			end
 		end
 	end
+	return game
 end
 
 return game
