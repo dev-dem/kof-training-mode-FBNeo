df97476a713934a052b2eb97baf5327cd0df1fe0
Demian <demianmcaceres@gmail.com>
2021-02-21 03:21:56 -0300

    Added file to lib folder Added function to catch unsopported games

--- lib/game.lua
+++ lib/game.lua
@@ -1,7 +1,16 @@
 local game = {}
 
+function game.isSupported(game)
+	local supported = true
+	if fn.isEmptyTable(game) then
+		supported = false
+		print("Game not supported")
+	end
+	return supported 
+end
+
 function game.getData()
-	local game = nil
+	local game = {}
 	for _, module in ipairs(profile.get()) do
 		for _, shortname in ipairs(module.name) do
 			if emu.romname() == shortname or emu.parentname() == shortname then
